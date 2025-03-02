04c1299b7fa994e71d5eaf3c4e3ebf958a0d6837
Korzhavin Ivan <korjavin@gmail.com>
2025-03-02 14:09:44 +0100

    Remove bingo feature

--- telegram.go
+++ telegram.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"log"
@@ -12,7 +11,6 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/olekukonko/tablewriter"
 )
 
 type class struct {
@@ -38,7 +36,6 @@ func botGo(filter filterFunc) {
 	name := me.UserName
 
 	bot.Debug = false
-	sg := make(map[int]*SheetGenerator)
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -192,80 +189,6 @@ func botGo(filter filterFunc) {
 			}
 		}
 
-		if strings.HasPrefix(strings.ToUpper(text), "/NEWBINGO") {
-			var words []string
-			split := strings.Split(text, " ")
-			if len(split) > 1 {
-				words = append(words, split[1:]...)
-			}
-
-			sg[messg.From.ID] = NewSheetGenerator(words, time.Now().Unix())
-			answer := fmt.Sprintf("New bingo generator created. \n There are %d words in it\n You can add new with /ADD word command", len(words))
-			msg := tgbotapi.NewMessage(messg.Chat.ID, answer)
-			msg.ReplyToMessageID = messg.MessageID
-			_, err := bot.Send(msg)
-			if err != nil {
-				log.Printf("Send: %v ", err)
-			}
-		}
-		if strings.HasPrefix(strings.ToUpper(text), "/ADD") {
-			var answer string
-			if _, ok := sg[messg.From.ID]; !ok {
-				sg[messg.From.ID] = NewSheetGenerator([]string{}, time.Now().Unix())
-			}
-			var words []string
-			split := strings.Split(text, " ")
-			if len(split) > 1 {
-				words = append(words, split[1:]...)
-				for _, w := range words {
-					sg[messg.From.ID].AddWord(w)
-				}
-				answer = fmt.Sprintf("%d words added\n Thank you. You can use /BINGO now", len(words))
-			} else {
-				answer = "Give me a word or two to add"
-			}
-			msg := tgbotapi.NewMessage(messg.Chat.ID, answer)
-			msg.ReplyToMessageID = messg.MessageID
-			_, err := bot.Send(msg)
-			if err != nil {
-				log.Printf("Send: %v ", err)
-			}
-
-		}
-		if strings.HasPrefix(strings.ToUpper(text), "/BINGO") {
-			var answer string
-			if _, ok := sg[messg.From.ID]; !ok {
-				answer = "I don't have words for you. Create own by /NEWBINGO"
-			} else {
-				split := strings.Split(text, " ")
-				var size int
-				if len(split) > 1 {
-					size, _ = strconv.Atoi(split[1])
-				}
-				words := sg[messg.From.ID].GenerateOne(size)
-				var captions []string
-				for range words[0] {
-					captions = append(captions, "-")
-				}
-
-				buf := bytes.NewBufferString("```")
-				table := tablewriter.NewWriter(buf)
-				table.SetHeader(captions)
-				table.AppendBulk(words)
-				table.Render()
-				buf.WriteString("```")
-				answer = buf.String()
-			}
-
-			msg := tgbotapi.NewMessage(messg.Chat.ID, answer)
-			msg.ParseMode = "Markdown"
-			// msg.ReplyToMessageID = messg.MessageID
-			_, err := bot.Send(msg)
-			if err != nil {
-				log.Printf("Send: %v ", err)
-			}
-
-		}
 		if strings.HasPrefix(strings.ToUpper(text), "/HELP") {
 			answer := `You can send me any text to read aloud, but please mention me by @` + name +
 				` /oxford term :  show the definition from Oxford dictionary
